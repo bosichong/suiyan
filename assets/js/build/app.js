11795c28adedb9f607423f3783fedafcce63c5d7
J_sky <bosichong@qq.com>
2020-01-03 19:20:34 +0800

    优化了JS文件的合并

--- assets/js/build/app.js
+++ assets/js/build/app.js
@@ -210,7 +210,109 @@ suiyan.getQueryVariable = function (variable) {
 // }
 
 
+/**
+ * 站点页面模板
+ */
+
+// 左侧导航导航
+// loadHtml('./templates/side.html','lt');//JavaScript原生AJAX
 $(document).ready(function () {
+
+    // footer.html
+    $(".footer").load("assets/templates/footer.html", function (response, status, request) {
+        if (status == "success")
+            console.warn("如果你能看到这里说明你已经很牛逼撩！欢迎进群讨论学习Q群:217840699");
+
+    });
+
+    // footer.html
+    $(".header").load("assets/templates/side.html", function (response, status, request) {
+        if (status == "success")
+            console.warn("如果你能看到这里说明你已经很牛逼撩！欢迎进群讨论学习Q群:217840699");
+
+    });
+
+
+
+    //加载blog配置数据,并填充数据到页面
+    $.getJSON("config.json", function (data, textStatus, jqXHR) {
+        // alert(data.blog_name);
+        //站点信息填充
+        // $('#blogcss').attr("href", "assets/css/"+data.blogcss+".css");
+        // $('#highlight').attr("href", "assets/plugins/highlight/styles/"+data.highlight+".css");
+
+        suiyan.config = data;
+        $("title").text(data.blog_name + data.meta_description);
+        $("meta[name='description']").attr("content", data.meta_description);
+        $("meta[name='author']").attr("content", data.blog_author);
+        $("meta[name='keywords']").attr("content", data.blog_keywords);
+        //blog基本信息
+        $(".blog-name a").text(data.blog_name); //bolg名称
+        $(".blog-description").text(data.blog_description);
+        $('.profile-image').attr("src", data.profile_image);
+
+
+
+
+        //循环添加SNS
+        (function (data, ul) {
+            for (let i = 0; i < data.length; i++) {
+                var lihtml = $('<li><a class="hide" href="' + data[i].url + '" target="blank_blank"><i class="fa fa-' + data[i].ico + ' fa-lg"></li></a></i>');
+                ul.append(lihtml);
+
+            }
+
+        })(data.blog_sns, $(".social-list"));
+
+        //循环添加导航
+        (function (data, ul) {
+            for (let i = 0; i < data.length; i++) {
+                var lihtml = $('<li class="nav-item hide"><a class="nav-link" href="' + data[i].url + '"><i class="fa fa-' + data[i].ico + ' fa-lg"></i> ' + data[i].text + '</a></li>');
+                ul.append(lihtml);
+
+            }
+
+        })(data.nav, $(".blog-nav"));
+
+        //隐藏元素的动画
+        var scon = 100
+        $('.hide').each(function (index, element) {
+            $(this).fadeIn(scon);
+            scon += 100;
+
+        });
+
+    });
+
+
+    $.getJSON("blog_data.json", function (blogdata, textStatus, jqXHR) {
+        suiyan.blog_data = blogdata;
+
+        // 搜索按钮
+        $('.search').click(function (e) {
+            var key = $('#skey').val();
+            var sdata = suiyan.getsearch(suiyan.blog_data, key)
+            var shtmlstr = '';
+            if (sdata != '') {
+                shtmlstr += '<ul class="car-list navbar-nav">'
+                for (let index = 0; index < sdata.length; index++) {
+                    const el = sdata[index];
+                    shtmlstr += '<li class="list-group-item"><a href="p.html?p=' + el.url + '">' + el.title + '</a> <span title="发布日期">' + el.time + '</span></li>';
+                }
+                shtmlstr += '</ul>'
+            } else {
+                shtmlstr = '<p>没有搜到任何结果哦！</p>'
+            }
+            $('.search-list').html(shtmlstr);
+
+        });
+
+    });
+
+
+
+
+});$(document).ready(function () {
 
     $.getJSON("blog_data.json", function (data, textStatus, jqXHR) {
         // console.log(suiyan.formatData(data));
@@ -406,109 +508,7 @@ $(document).ready(function () {
 
 
 });
-/**
- * 站点页面模板
- */
-
-// 左侧导航导航
-// loadHtml('./templates/side.html','lt');//JavaScript原生AJAX
 $(document).ready(function () {
-
-    // footer.html
-    $(".footer").load("assets/templates/footer.html", function (response, status, request) {
-        if (status == "success")
-            console.warn("如果你能看到这里说明你已经很牛逼撩！欢迎进群讨论学习Q群:217840699");
-
-    });
-
-    // footer.html
-    $(".header").load("assets/templates/side.html", function (response, status, request) {
-        if (status == "success")
-            console.warn("如果你能看到这里说明你已经很牛逼撩！欢迎进群讨论学习Q群:217840699");
-
-    });
-
-
-
-    //加载blog配置数据,并填充数据到页面
-    $.getJSON("config.json", function (data, textStatus, jqXHR) {
-        // alert(data.blog_name);
-        //站点信息填充
-        // $('#blogcss').attr("href", "assets/css/"+data.blogcss+".css");
-        // $('#highlight').attr("href", "assets/plugins/highlight/styles/"+data.highlight+".css");
-
-        suiyan.config = data;
-        $("title").text(data.blog_name + data.meta_description);
-        $("meta[name='description']").attr("content", data.meta_description);
-        $("meta[name='author']").attr("content", data.blog_author);
-        $("meta[name='keywords']").attr("content", data.blog_keywords);
-        //blog基本信息
-        $(".blog-name a").text(data.blog_name); //bolg名称
-        $(".blog-description").text(data.blog_description);
-        $('.profile-image').attr("src", data.profile_image);
-
-
-
-
-        //循环添加SNS
-        (function (data, ul) {
-            for (let i = 0; i < data.length; i++) {
-                var lihtml = $('<li><a class="hide" href="' + data[i].url + '" target="blank_blank"><i class="fa fa-' + data[i].ico + ' fa-lg"></li></a></i>');
-                ul.append(lihtml);
-
-            }
-
-        })(data.blog_sns, $(".social-list"));
-
-        //循环添加导航
-        (function (data, ul) {
-            for (let i = 0; i < data.length; i++) {
-                var lihtml = $('<li class="nav-item hide"><a class="nav-link" href="' + data[i].url + '"><i class="fa fa-' + data[i].ico + ' fa-lg"></i> ' + data[i].text + '</a></li>');
-                ul.append(lihtml);
-
-            }
-
-        })(data.nav, $(".blog-nav"));
-
-        //隐藏元素的动画
-        var scon = 100
-        $('.hide').each(function (index, element) {
-            $(this).fadeIn(scon);
-            scon += 100;
-
-        });
-
-    });
-
-
-    $.getJSON("blog_data.json", function (blogdata, textStatus, jqXHR) {
-        suiyan.blog_data = blogdata;
-
-        // 搜索按钮
-        $('.search').click(function (e) {
-            var key = $('#skey').val();
-            var sdata = suiyan.getsearch(suiyan.blog_data, key)
-            var shtmlstr = '';
-            if (sdata != '') {
-                shtmlstr += '<ul class="car-list navbar-nav">'
-                for (let index = 0; index < sdata.length; index++) {
-                    const el = sdata[index];
-                    shtmlstr += '<li class="list-group-item"><a href="p.html?p=' + el.url + '">' + el.title + '</a> <span title="发布日期">' + el.time + '</span></li>';
-                }
-                shtmlstr += '</ul>'
-            } else {
-                shtmlstr = '<p>没有搜到任何结果哦！</p>'
-            }
-            $('.search-list').html(shtmlstr);
-
-        });
-
-    });
-
-
-
-
-});$(document).ready(function () {
     $.getJSON("blog_data.json", function (data, textStatus, jqXHR) {
         suiyan.tagsjson = suiyan.format_tags_data(data);
         // console.log(suiyan.tagsjson);
